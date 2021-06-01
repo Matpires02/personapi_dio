0d74915f386cfdfe567d39d83511e85a54506007
Matheus Pires <61023277+Matpires02@users.noreply.github.com>
2021-06-01 19:58:31 -0300

    Developed person create operation

---
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -0,0 +1,29 @@
+package one.digitalinnovation.personapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.digitalinnovation.personapi.enums.PhoneType;
+
+import javax.persistence.*;
+
+@Entity
+@Data //insere getters e seters
+@Builder
+@AllArgsConstructor //insere construtores
+@NoArgsConstructor //insere construtores
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false)
+    private String number;
+
+}
