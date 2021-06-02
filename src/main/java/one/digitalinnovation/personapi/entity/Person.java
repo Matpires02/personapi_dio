857fa953c9b9513a5aa2a89ee2507f9df8ff1f81
Matheus Pires <61023277+Matpires02@users.noreply.github.com>
2021-06-01 22:38:08 -0300

    Inproved person creation functionality

--- src/main/java/one/digitalinnovation/personapi/entity/Person.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Person.java
@@ -5,7 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -21,16 +28,16 @@ public class Person {
     private Long id;
 
     @Column(nullable = false)
-    private String FirstName;
+    private String firstName;
 
     @Column(nullable = false)
-    private String LastName;
+    private String lastName;
 
-    @Column(nullable = false, unique = true)//define como nao nulo e unico
+    @Column(nullable = false, unique = true)
     private String cpf;
 
     private LocalDate birthDate;
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST,CascadeType.MERGE,CascadeType.REMOVE})
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
     private List<Phone> phones;
 }
